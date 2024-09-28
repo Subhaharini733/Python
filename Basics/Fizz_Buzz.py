e91c16ddb32adc7ac794793574f5659d070f9751
Subhaharini733 <subhaharini733@gmail.com>
2024-09-28 11:54:15 +0530

    Update Fizz_Buzz.py

--- Basics/Fizz_Buzz.py
+++ Basics/Fizz_Buzz.py
@@ -17,4 +17,12 @@ class Solution:
             output.append(ans)
         return output
 
+# we are asked to submit output in list , output list is created.
+# lets loop to get every number of n.
+# check if i is divisible by 3 , if yes then store 'Fizz' in ans and 
+#    then check for 5, if yes append 'Buzz' to ans.
+# then in elif check if i divisible by 5 alone, if yes store 'Buzz' to ans .
+# else store string of i in ans (for 1, 2 , 4, 6, 7....)
+# then append everything to output and return output.
+    
         

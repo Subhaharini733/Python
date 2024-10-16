02218acdf8617acb857c13aafc08ada21141e8c1
Subhaharini733 <subhaharini733@gmail.com>
2024-10-16 16:58:59 +0530

    Create Even_or_odd.py

---
+++ Geeks_for_Geeks/Even_or_odd.py
@@ -0,0 +1,12 @@
+# https://www.geeksforgeeks.org/problems/odd-or-even3618/1
+time comp: O(1)
+space comp: O(1)
+
+class Solution:
+    def oddEven(self, n):
+        return 'even' if n % 2 == 0 else 'odd'
+
+if __name__ == '__main__':
+    N = int(input())
+    ob = Solution()
+    print(ob.oddEven(N))
